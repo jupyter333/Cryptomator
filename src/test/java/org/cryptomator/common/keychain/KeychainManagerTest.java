c54698d91ec2a8fcfa2fb96999cd1da3348cd4f7
Cesar Rodriguez <cesaraumon@hotmail.com>
2024-09-29 23:42:58 -0400

    roll back

--- src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
+++ src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
@@ -54,27 +54,7 @@ public class KeychainManagerTest {
 			Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> latch.await());
 			Assertions.assertTrue(result.get());
 		}
-		@Test
-		public void testNoKeychainAccessProviderException() {
-			KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(null));
-			Assertions.assertThrows(NoKeychainAccessProviderException.class, () -> {
-				keychainManager.storePassphrase("key", "display", "passphrase");
-			});
-		}
-		@Test
-		public void testIsPassphraseStoredReturnsFalseWhenNull() throws KeychainAccessException {
-			KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
-			char[] result = keychainManager.loadPassphrase("non-existent-key");
-			Assertions.assertNull(result);
-			Assertions.assertFalse(keychainManager.isPassphraseStored("non-existent-key"));
-		}
-		@Test
-		public void testDeletePassphrase() throws KeychainAccessException {
-			KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
-			keychainManager.storePassphrase("test-key", "test-display", "test-pass");
-			keychainManager.deletePassphrase("test-key");
-			Assertions.assertFalse(keychainManager.isPassphraseStored("test-key"));
-		}
+
 
 	}
 
