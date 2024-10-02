206630caf07cbf59059f0d8350eab4b2454e0c95
Jho <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-10-01 21:38:13 -0400

    added a dummy test, checking if github actions recognize it

--- src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
+++ src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
@@ -26,6 +26,14 @@ public class KeychainManagerTest {
 		Assertions.assertArrayEquals("asd".toCharArray(), keychainManager.loadPassphrase("test"));
 	}
 
+	@Test
+	public void testIsSupported() {
+		KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
+		Assertions.assertTrue(keychainManager.isSupported());
+		keychainManager = new KeychainManager(new SimpleObjectProperty<>(null));
+		Assertions.assertFalse(keychainManager.isSupported());
+	}
+
 	@Nested
 	public static class WhenObservingProperties {
 
@@ -55,27 +63,6 @@ public class KeychainManagerTest {
 			Assertions.assertTrue(result.get());
 		}
 
-	}
-	@Test
-	public void testNoKeychainAccessProviderException() {
-		KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(null));
-		Assertions.assertThrows(NoKeychainAccessProviderException.class, () -> {
-			keychainManager.storePassphrase("key", "display", "passphrase");
-		});
-	}
-	@Test
-	public void testIsPassphraseStoredReturnsFalseWhenNull() throws KeychainAccessException {
-		KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
-		char[] result = keychainManager.loadPassphrase("non-existent-key");
-		Assertions.assertNull(result);
-		Assertions.assertFalse(keychainManager.isPassphraseStored("non-existent-key"));
-	}
-	@Test
-	public void testDeletePassphrase() throws KeychainAccessException {
-		KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
-		keychainManager.storePassphrase("test-key", "test-display", "test-pass");
-		keychainManager.deletePassphrase("test-key");
-		Assertions.assertFalse(keychainManager.isPassphraseStored("test-key"));
 	}
 
 
