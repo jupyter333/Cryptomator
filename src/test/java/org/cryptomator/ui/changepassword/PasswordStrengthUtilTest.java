05da5b66197a06ec788a80b30f39125d2352055a
Jho <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-10-02 12:20:25 -0400

    added 3 tests to cover  getStrengthDescription(Number score) in PasswordStrengthUtil.java

--- src/test/java/org/cryptomator/ui/changepassword/PasswordStrengthUtilTest.java
+++ src/test/java/org/cryptomator/ui/changepassword/PasswordStrengthUtilTest.java
@@ -30,4 +30,61 @@ public class PasswordStrengthUtilTest {
 		Assertions.assertEquals(4, result2);
 	}
 
+	// Nouveau test jhosim
+	@Test
+	public void testGetStrengthDescriptionTooShort(){
+
+		String RESSOURCE_PREFIX = "passwordStrength.messageLabel.";
+
+		ResourceBundle fakeBundle = Mockito.mock(ResourceBundle.class);
+		Environment fakeEnvironment = Mockito.mock(Environment.class);
+
+		PasswordStrengthUtil util = new PasswordStrengthUtil(fakeBundle, fakeEnvironment);
+
+		Mockito.when(fakeBundle.getString(RESSOURCE_PREFIX + "tooShort")).thenReturn("too short bro");
+		Mockito.when(fakeEnvironment.getMinPwLength()).thenReturn(10);
+
+		String test = util.getStrengthDescription(-1);
+
+		Assertions.assertEquals("too short bro", test);
+
+	}
+
+	@Test
+	public void testGetStrengthDescriptionValid(){
+
+		String RESSOURCE_PREFIX = "passwordStrength.messageLabel.";
+
+		ResourceBundle fakeBundle = Mockito.mock(ResourceBundle.class);
+		Environment fakeEnvironment = Mockito.mock(Environment.class);
+
+		PasswordStrengthUtil util = new PasswordStrengthUtil(fakeBundle, fakeEnvironment);
+
+		Mockito.when(fakeBundle.containsKey(RESSOURCE_PREFIX + "11")).thenReturn(true);
+		Mockito.when(fakeBundle.getString(RESSOURCE_PREFIX+"11")).thenReturn("long enough bro");
+
+		String test = util.getStrengthDescription(11);
+
+		Assertions.assertEquals("long enough bro", test);
+
+	}
+
+	@Test
+	public void testGetStrengthDescriptionNoKey(){
+
+		String RESSOURCE_PREFIX = "passwordStrength.messageLabel.";
+
+		ResourceBundle fakeBundle = Mockito.mock(ResourceBundle.class);
+		Environment fakeEnvironment = Mockito.mock(Environment.class);
+
+		PasswordStrengthUtil util = new PasswordStrengthUtil(fakeBundle, fakeEnvironment);
+
+		Mockito.when(fakeBundle.containsKey(RESSOURCE_PREFIX + "4")).thenReturn(false);
+
+		String test = util.getStrengthDescription(4);
+
+		Assertions.assertEquals("", test);
+
+	}
+
 }
