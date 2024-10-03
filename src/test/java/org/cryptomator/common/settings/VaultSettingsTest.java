e51ab1f61cfa90c1d92793a2f2377246c0682ba7
aerolitoDesk <cesaraumon@hotmail.com>
2024-10-03 01:27:38 -0400

    2 tests

--- src/test/java/org/cryptomator/common/settings/VaultSettingsTest.java
+++ src/test/java/org/cryptomator/common/settings/VaultSettingsTest.java
@@ -10,6 +10,9 @@ package org.cryptomator.common.settings;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,5 +30,48 @@ public class VaultSettingsTest {
 	public void testNormalize(String test, String expected) {
 		assertEquals(expected, VaultSettings.normalizeDisplayName(test));
 	}
+	private VaultSettings vaultSettings;
 
+	@BeforeEach
+	void setUp() {
+		// dummy settings du vault
+		VaultSettingsJson json = new VaultSettingsJson();
+		json.id = "testId";
+		json.path = "/test/path";
+		json.displayName = "Test Vault";
+		json.unlockAfterStartup = true;
+		json.revealAfterMount = true;
+		json.usesReadOnlyMode = false;
+		json.mountFlags = "rw";
+		json.maxCleartextFilenameLength = 255;
+		json.actionAfterUnlock = WhenUnlocked.ASK;
+		json.autoLockWhenIdle = true;
+		json.autoLockIdleSeconds = 600;
+		json.mountPoint = "/mount/point";
+		json.mountService = "testService";
+		json.port = 42427;
+
+		vaultSettings = new VaultSettings(json);
+	}
+
+	@Test
+	public void testSerialized() {
+
+		VaultSettingsJson serializedJson = vaultSettings.serialized();
+
+		assertEquals("testId", serializedJson.id);
+		assertEquals("/test/path", serializedJson.path);
+		assertEquals("Test Vault", serializedJson.displayName);
+		assertTrue(serializedJson.unlockAfterStartup);
+		assertTrue(serializedJson.revealAfterMount);
+		assertFalse(serializedJson.usesReadOnlyMode);
+		assertEquals("rw", serializedJson.mountFlags);
+		assertEquals(255, serializedJson.maxCleartextFilenameLength);
+		assertEquals(WhenUnlocked.ASK, serializedJson.actionAfterUnlock);
+		assertTrue(serializedJson.autoLockWhenIdle);
+		assertEquals(600, serializedJson.autoLockIdleSeconds);
+		assertEquals("/mount/point", serializedJson.mountPoint);
+		assertEquals("testService", serializedJson.mountService);
+		assertEquals(42427, serializedJson.port);
+	}
 }
